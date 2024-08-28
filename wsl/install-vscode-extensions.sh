54f2d334dac0eeb76a235301950b669f84c0cc6a
Bernardo Euler <bernardoeulersantos@gmail.com>
2024-08-28 14:56:31 -0300

    Create shell scripts to install/uninstall VS Code extensions on WSL #5

---
+++ wsl/install-vscode-extensions.sh
@@ -0,0 +1,5 @@
+code --install-extension ms-python.python --force
+code --install-extension ms-python.vscode-pylance --force
+code --install-extension naumovs.color-highlight --force
+code --install-extension ritwickdey.liveserver --force
+code --install-extension visualstudioexptteam.vscodeintellicode --force
---
+++ wsl/uninstall-vscode-extensions.sh
@@ -0,0 +1,5 @@
+code --uninstall-extension ms-python.python --force
+code --uninstall-extension ms-python.vscode-pylance --force
+code --uninstall-extension naumovs.color-highlight --force
+code --uninstall-extension ritwickdey.liveserver --force
+code --uninstall-extension visualstudioexptteam.vscodeintellicode --force
