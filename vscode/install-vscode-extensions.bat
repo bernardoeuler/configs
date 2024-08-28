e1a040595b8abf32b2aca65f2687864c290cccce
Bernardo Euler <bernardoeulersantos@gmail.com>
2024-08-28 15:18:04 -0300

    Create batch script to install VS Code extensions on Windows #3

---
+++ vscode/install-vscode-extensions.bat
@@ -0,0 +1,10 @@
+call code --install-extension brandonkirbyson.vscode-animations --force
+call code --install-extension drcika.apc-extension --force
+call code --install-extension hilalh.hyper-dracula-vscode-theme --force
+call code --install-extension miguelsolorio.symbols --force
+call code --install-extension ms-python.python --force
+call code --install-extension ms-python.vscode-pylance --force
+call code --install-extension ms-vscode-remote.remote-wsl --force
+call code --install-extension naumovs.color-highlight --force
+call code --install-extension ritwickdey.liveserver --force
+call code --install-extension visualstudioexptteam.vscodeintellicode --force
